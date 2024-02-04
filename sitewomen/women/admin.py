0ac04912c66f89c0ec6ebc54ebfd1bb811a0b3f9
User Alexander <tyrbomaks@mail.ru>
2024-02-04 07:15:26 +0300

    draft function on admin

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Women, Category
 
 
@@ -11,14 +11,21 @@ class WomenAdmin(admin.ModelAdmin):
 #n.site.register(Women, WomenAdmin) лучшая практика, через декоратор
     list_editable = ('is_published', ) #для редактирования записей
     list_per_page = 5
-    actions = ['set_published']
+    actions = ['set_published', 'set_draft']
 
     @admin.display(description="Краткое описание", ordering="content")
     def brief_info(self, women: Women):
         return f"Описание {len(women.content)} символов."
     @admin.action(description='Опубликовать выбранные записи')
     def set_published(self, request, queryset):
-        queryset.update(is_published=Women.Status.PUBLISHED)
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f'Изменено {count} записей.')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f'{count} записей сняты с публикации!.', messages.WARNING)
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
